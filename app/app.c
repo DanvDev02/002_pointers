994b0425b90aeacd39d4bbfc7290b5c2312fbf1b
Your Name <you@example.com>
2022-08-28 22:42:22 -0500

    Correcion del Frecuency

--- app/app.c
+++ app/app.c
@@ -3,8 +3,8 @@ int main(void)
 {
   int colleccion[5] = {25,58,25,15,8};
 
-  //bubble_sort(colleccion,5);
-  //insertion_sort(colleccion,5);
+  bubble_sort(colleccion,5);
+  insertion_sort(colleccion,5);
   reverse(colleccion,5);
   calulate_frequency(colleccion,5,25);
 
